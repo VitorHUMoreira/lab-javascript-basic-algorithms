40c60eaed81805a6e1f5155e80a42fa97a35b156
VitorHUMoreira <vitorhugo_187@hotmail.com>
2022-08-10 11:18:03 -0300

    iteration 3 done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,30 @@
-// Iteration 1: Names and Input
-//
-// Iteration 2: Conditionals
-//
-// Iteration 3: Loops
+const driver = "Vitor";
+const navigator = "Vitor Hugo";
 
-console.log("I'm ready");
+console.log(`The driver's name is ${driver}`);
+console.log(`The navigator's name is ${navigator}`);
+
+if (driver.length > navigator.length) {
+  console.log(
+    `The driver has the longest name, ${driver} has ${driver.length} characters.`
+  );
+} else if (navigator.length > driver.length) {
+  console.log(
+    `It seems that the navigator has the longest name, ${navigator} has ${navigator.length} characters.`
+  );
+} else {
+  console.log(
+    `Wow, you both have equally long names, ${driver} and ${navigator} has ${driver.length} characters!`
+  );
+}
+
+const driverName = driver
+  .toUpperCase()
+  .split("")
+  .join(" ")
+  .replace(/\s+/g, " ");
+
+const navigatorName = navigator.split("").reverse().join("");
+
+console.log(`Driver's name: ${driverName}`);
+console.log(`Navigator's name: ${navigatorName}`);
